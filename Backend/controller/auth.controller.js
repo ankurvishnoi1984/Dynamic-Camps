f75a85c5c16634f3d89049632527adaa91b320f9
shailendra-k1245 <iwilldofine@gmail.com>
2025-10-16 18:56:35 +0530

    auth api

---
+++ Backend/controller/auth.controller.js
@@ -0,0 +1,211 @@
+
+const db = require("../config/db")
+const logger = require('../utils/logger')
+
+
+exports.loginWithEmpCode = async (req, res) => {
+  const { email, password } = req.body;
+  const query = 'select user_id, empcode, password, email, role, designation from user_mst where empcode=?';
+
+  try {
+    db.query(query, [email], (err, result) => {
+      if (err) {
+        logger.error(`Error in /controller/auth/login: ${err.message}`);
+        return res.status(500).json({
+          errorCode: "0",
+          errorDetail: err.message,
+          responseData: {},
+          status: "ERROR",
+          details: "An internal server error occurred",
+          getMessageInfo: "An internal server error occurred"
+        });
+      } else if (result.length === 0) {
+        return res.status(401).json({
+          errorCode: "0",
+          errorDetail: "Invalid Email or Password",
+          responseData: {},
+          status: "ERROR",
+          details: "UNAUTHORIZED",
+          getMessageInfo: "Invalid Email or Password"
+        });
+      } else {
+        const user = result[0];
+        if (password == user.password) {
+          const loginTime = new Date();
+          const historyQuery = 'insert into user_login_history (user_id, login_datetime) values (?, ?)';
+          db.query(historyQuery, [user.user_id, loginTime], (err, result) => {
+            if (err) {
+              logger.error(`Error in /controller/auth/login: ${err.message}`);
+              return res.status(500).json({
+                errorCode: "0",
+                errorDetail: err.message,
+                responseData: {},
+                status: "ERROR",
+                details: "An internal server error occurred",
+                getMessageInfo: "An internal server error occurred"
+              });
+            } else {
+              const historyId = result.insertId;
+              return res.json({
+                errorCode: "1",
+                errorDetail: "",
+                responseData: {
+                  message: "Login successful",
+                  empId: user.empcode,
+                  user_id: user.user_id,
+                  role: user.role,
+                  sessionID:historyId,
+                  designation:user.designation,
+                },
+                status: "SUCCESS",
+                details: "",
+                getMessageInfo: ""
+              });
+            }
+          });
+        } else {
+          return res.status(401).json({
+            errorCode: "UNAUTHORIZED",
+            errorDetail: "Invalid Email or Password",
+            responseData: {},
+            status: "ERROR",
+            details: "Invalid Email or Password",
+            getMessageInfo: "Invalid Email or Password"
+          });
+        }
+      }
+    });
+  } catch (error) {
+    logger.error(error.message);
+    return res.status(500).json({
+      errorCode: "0",
+      errorDetail: error.message,
+      responseData: {},
+      status: "ERROR",
+      details: "An internal server error occurred",
+      getMessageInfo: "An internal server error occurred"
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { usernamehq, password } = req.body;  // changed from email to usernamehq
+  const query = 'SELECT user_id, usernamehq,empcode, password, role, designation, name FROM user_mst WHERE usernamehq=? AND status="Y"';
+
+  try {
+    db.query(query, [usernamehq], (err, result) => {
+      if (err) {
+        logger.error(`Error in /controller/auth/login: ${err.message}`);
+        return res.status(500).json({
+          errorCode: "0",
+          errorDetail: err.message,
+          responseData: {},
+          status: "ERROR",
+          details: "An internal server error occurred",
+          getMessageInfo: "An internal server error occurred"
+        });
+      } else if (result.length === 0) {
+        return res.status(401).json({
+          errorCode: "0",
+          errorDetail: "Invalid Username or Password",
+          responseData: {},
+          status: "ERROR",
+          details: "UNAUTHORIZED",
+          getMessageInfo: "Invalid Username or Password"
+        });
+      } else {
+        const user = result[0];
+
+        if (password == user.password) {  // Plaintext check, consider hashing later
+          const loginTime = new Date();
+          const historyQuery = 'INSERT INTO user_login_history (user_id, login_datetime) VALUES (?, ?)';
+
+          db.query(historyQuery, [user.user_id, loginTime], (err, result) => {
+            if (err) {
+              logger.error(`Error in /controller/auth/login: ${err.message}`);
+              return res.status(500).json({
+                errorCode: "0",
+                errorDetail: err.message,
+                responseData: {},
+                status: "ERROR",
+                details: "An internal server error occurred",
+                getMessageInfo: "An internal server error occurred"
+              });
+            } else {
+              const historyId = result.insertId;
+              return res.json({
+                errorCode: "1",
+                errorDetail: "",
+                responseData: {
+                  message: "Login successful",
+                  usernamehq: user.usernamehq,
+                  user_id: user.user_id,
+                  empId: user.empcode,
+                  role: user.role,
+                  designation: user.designation,
+                  sessionID: historyId,
+                  name: user.name,
+                },
+                status: "SUCCESS",
+                details: "",
+                getMessageInfo: ""
+              });
+            }
+          });
+        } else {
+          return res.status(401).json({
+            errorCode: "0",
+            errorDetail: "Invalid Username or Password",
+            responseData: {},
+            status: "ERROR",
+            details: "Invalid Username or Password",
+            getMessageInfo: "Invalid Username or Password"
+          });
+        }
+      }
+    });
+  } catch (error) {
+    logger.error(error.message);
+    return res.status(500).json({
+      errorCode: "0",
+      errorDetail: error.message,
+      responseData: {},
+      status: "ERROR",
+      details: "An internal server error occurred",
+      getMessageInfo: "An internal server error occurred"
+    });
+  }
+};
+exports.logout = async (req, res) => {
+  const {sessionId } = req.body;
+  const logoutTime = new Date();
+  const query = 'update user_login_history set logout_datetime =? where lh_id = ?'
+  try {
+    db.query(query, [logoutTime,sessionId], (err, result) => {
+      if (err) {
+      logger.error(err.message);
+
+        res.status(500).json({
+          errorCode: "0",
+          errorDetail: err.message,
+          responseData: {},
+          status: "ERROR",
+          details: "An internal server error occurred",
+          getMessageInfo: "An internal server error occurred"
+        });
+      }
+      else{
+        res.status(200).json({ message: "Logout Successfully",errorCode: "1"})
+      } 
+    });
+  } catch (error) {
+    logger.error(error.message);
+    res.send(error)
+  }
+};
+
+
+
+
+
+
---
+++ Backend/routes/auth.route.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const {login, logout} = require("../controller/auth");
+const { loginWithEmpCode } = require("../controller/auth.controller");
+const router = express.Router();
+
+router.post("/login",loginWithEmpCode)
+router.post("/logout",logout)
+
+
+module.exports= router;
