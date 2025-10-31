76afef6b844caa63a833e51a7142a9cf7e89a2dc
shailendra-k1245 <iwilldofine@gmail.com>
2025-10-31 11:29:07 +0530

    recent camp data for admin

--- SuperAdminPortal/src/components/dashboard/dashboard.jsx
+++ SuperAdminPortal/src/components/dashboard/dashboard.jsx
@@ -36,11 +36,7 @@ function Dashboard() {
 
   const [addUserModel, setAddUserModel] = useState(false)
 
-  const [catValue, setCatValue] = useState('');
-
   const [dashboardSummary, setDashSummary] = useState([]);
-
-  const [myCampType,setMyCampType] = useState([]);
 
     const [filters, setFilters] = useState({
       fromDate: "",
@@ -48,17 +44,6 @@ function Dashboard() {
       campType: ""
     });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFilters((prev) => ({
-      ...prev,
-      [name]: value
-    }));
-  };
-
-  const handelSubCatChange = (e) => {
-    setCatValue(e.target.value)
-  }
 
   const [totals, setTotals] = useState({
    totalClient:0,
@@ -66,43 +51,6 @@ function Dashboard() {
    totalEmp:0,
    totalCamps:0
   });
-
-  // useEffect(() => {
-  //   if (!dashboardSummary || dashboardSummary.length === 0) {
-  //     setTotals({
-  //       totalClient: 0,
-  //       totalDept: 0,
-  //       totalEmp: 0,
-  //       totalCamps: 0,
-  //     });
-  //     return;
-  //   }
-
-  //   // take doctor total directly from API (first row)
-  //   const empTotal = recentClients.totalEmployees;
-
-  //   // sum the other fields across rows
-  //   const summed = recentClients.reduce(
-  //     (acc, item) => {
-  //       acc.totalClient += item.totalClient || 0;
-  //       acc.totalDept += Number(item.totalDept) || 0;
-  //       acc.totalApprovalPendingCount += Number(item.approval_pending) || 0;
-  //       acc.totalApprovalCompletedCount += Number(item.approval_completed )|| 0;
-  //       return acc;
-  //     },
-  //     {
-  //       totalCampCount: 0,
-  //       totalPrescriptionCount: 0,
-  //       totalApprovalPendingCount: 0,
-  //       totalApprovalCompletedCount: 0,
-  //     }
-  //   );
-
-  //   setTotals({
-  //     totalDoctorCount: doctorTotal,
-  //     ...summed,
-  //   });
-  // }, [dashboardSummary]);
 
 
   const handelCloseModel = () => {
@@ -123,20 +71,6 @@ function Dashboard() {
     setShowConfirmation(false);
   };
 
-  const getMyCampsType = async () => {
-    setLoading(true)
-    try {
-      const res = await axios.get(`${BASEURL2}/basic/getCampNonCampType`)
-      setRecentCamps(res.data.data)
-      console.log("res.data.data", res.data)
-    } catch (error) {
-      console.log(error)
-    }
-    finally {
-      setLoading(false);
-    }
-  }
-
     const getRecentClients = async () => {
     setLoading(true)
     try {
@@ -153,7 +87,7 @@ function Dashboard() {
   const getRecentCamps = async () => {
     setLoading(true)
     try {
-      const res = await axios.get(`${BASEURL2}/dashboard/getRecentCampDetails`)
+      const res = await axios.post(`${BASEURL2}/dashboard/getRecentCampDetails`)
       setRecentCamps(res.data.data)
       console.log("res.data.data", res.data)
     } catch (error) {
@@ -181,7 +115,6 @@ function Dashboard() {
 
   useEffect(() => {
     getSummaryByActivity();
-    getMyCampsType();
     getRecentClients();
     getRecentCamps();
     getTotalCountDetails();
@@ -196,90 +129,6 @@ function Dashboard() {
  
 };
 
-  async function GetCatData() {
-    try {
-      const res = await axios.get(`${BASEURL}/admin/getSubCatData`, {
-        params: {
-          empcode: loginEmpCode
-        }
-      })
-      SetCatData(res.data);
-
-
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-
-
-  const handelReportDownload = () => {
-    // Define custom column headers
-    const headers = [
-      'Category Name',
-      'Camp Count',
-      'Doctor Count',
-      'Prescription Count',
-    ];
-
-    // Map the data to match the custom column headers
-    const mappedData = dashboardSummary.map(item => ({
-      'Category Name': item.activity_name,
-      'Camp Count': item.total_camps,
-      'Doctor Count': item.totalDoctors,
-      'Prescription Count': item.total_prescriptions,
-    }));
-
-    const ws = XLSX.utils.json_to_sheet(mappedData, { header: headers });
-    const wb = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'Data');
-    XLSX.writeFile(wb, 'admin_dashboard_report.xlsx');
-
-  }
-
-
-  // const handelDataChange =(e)=>{
-  //    console.log(e.target.value)
-  // } 
-
-  const handelSingalReportDownload = (id) => {
-    const filterData = catData.filter((e) => {
-      return e.cat_id === id
-    })
-    // Define custom column headers
-    const headers = [
-      'Category Name',
-      'Camp Count',
-      'Doctor Count',
-      'Screened Count',
-      'Diagnosed Count',
-      'Prescription Count'
-    ];
-
-    // Map the data to match the custom column headers
-    const mappedData = filterData.map(item => ({
-      'Category Name': item.cat_name,
-      'Camp Count': item.camp_count,
-      'Doctor Count': item.doctor_count,
-      'Screened Count': item.screened_count,
-      'Diagnosed Count': item.diagnosed_count,
-      'Prescription Count': item.prescription_count
-    }));
-
-    const ws = XLSX.utils.json_to_sheet(mappedData, { header: headers });
-    const wb = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'Data');
-    XLSX.writeFile(wb, 'data.xlsx');
-  }
-
-  async function GetFilterData(filterBy) {
-    try {
-      const res = await axios.get(`${BASEURL}/admin/getSubCatFilterData?filter=${filterBy}`);
-      SetCatData(res.data)
-    } catch (error) {
-      console.log(error)
-    }
-  }
 
   const getSummaryByActivity = async () => {
       const payload = {
@@ -296,183 +145,6 @@ function Dashboard() {
       console.log(error)
     }
   }
-
-  const handleFilter = (e) => {
-    setFilter(e.target.value)
-    if (e.target.value) {
-
-      GetFilterData(e.target.value)
-    }
-    else {
-      GetCatData();
-    }
-
-  }
-
-
-
-
-
-
-  const [chartData, setChartData] = useState({
-    labels: [],
-    datasets: [{
-      label: 'Camp Count',
-      backgroundColor: [],
-      data: [],
-    }],
-  });
-
-  const options = {
-    scales: {
-      x: {
-        type: 'category',
-        labels: chartData.labels,
-      },
-      y: {
-        beginAtZero: true,
-        min: 0
-      },
-    },
-    plugins: {
-      legend: {
-        display: false // Show legend as text only
-      }
-    },
-    responsive: true, // Make the chart responsive
-    maintainAspectRatio: false, // Don't maintain aspect ratio for responsiveness
-    // title: {
-    //   display: true,
-    //   text: "Camp Added",
-    // },
-  };
-
-
-  const [chartData1, setChartData1] = useState({
-    labels: [],
-    datasets: [
-      {
-        label: 'Patients Screened',
-        backgroundColor: '#002D62',
-        borderColor: '#002D62',
-        borderWidth: 2,
-        data: [],
-      },
-      {
-        label: 'Patients Diagnosed',
-        backgroundColor: '#6CB4EE',
-        borderColor: '#6CB4EE',
-        borderWidth: 2,
-        data: [],
-      }],
-  });
-
-
-  const options1 = {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-    responsive: true, // Make the chart responsive
-    maintainAspectRatio: false, // Don't maintain aspect ratio for responsiveness
-  };
-  const monthNames = [
-    "January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-  ];
-
-  const convertMonthNumberToName = (monthNumber) => {
-    return monthNames[monthNumber - 1];
-  };
-
-  const getCampCount = async () => {
-
-    try {
-      const response = await axios.post(`${BASEURL}/admin/getCampCount`, { catValue });
-
-      console.log("doctor count response", response.data)
-
-      const sortedData = response.data.sort((a, b) => {
-        // If years are different, sort by year
-        if (a.report_year !== b.report_year) {
-          return a.report_year - b.report_year;
-        }
-        // If years are the same, sort by month
-        return a.report_month - b.report_month;
-      });
-
-      const xValues = sortedData.map(item => `${convertMonthNumberToName(item.report_month)} ${item.report_year}`);
-      const yValues = sortedData.map(item => item.Camp_Count);
-
-
-      setChartData({
-        labels: xValues,
-        datasets: [{
-          label: 'Camp Count',
-          backgroundColor: ["#EAB870", "#17a2b8", "#6CB4EE", "#6CB84D", "#B64DB8", "#6C4DB8"],
-          data: yValues,
-        }],
-      });
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-
-
-
-  const getScreenedCount = async () => {
-    try {
-      const response = await axios.post(`${BASEURL}/admin/getScreenedCount`, { catValue });
-      const response1 = await axios.post(`${BASEURL}/admin/getDiagnosedCount`, { catValue });
-
-      console.log("Survey data response", response.data);
-      console.log("Survey data response2", response1.data);
-
-      // Assuming response.data contains the same structure as your stacked bar chart data
-      const sortedData = response.data.sort((a, b) => {
-        // If years are different, sort by year
-        if (a.report_year !== b.report_year) {
-          return a.report_year - b.report_year;
-        }
-        // If years are the same, sort by month
-        return a.report_month - b.report_month;
-      });
-
-      const sortedData1 = response1.data.sort((a, b) => {
-        // If years are different, sort by year
-        if (a.report_year !== b.report_year) {
-          return a.report_year - b.report_year;
-        }
-        // If years are the same, sort by month
-        return a.report_month - b.report_month;
-      });
-
-      const xValues = sortedData.map(item => `${convertMonthNumberToName(item.report_month)} ${item.report_year}`);
-      const screened_count = sortedData.map(item => item.Screened_Count);
-      const daigoned_count = sortedData1.map(item => item.Daignosed_Count);
-
-      //const pendingValues = sortedData.map(item => item.survey_pending);
-      setChartData1({
-        labels: xValues,
-        datasets: [
-          { ...chartData1.datasets[0], data: screened_count },
-          { ...chartData1.datasets[1], data: daigoned_count },
-        ],
-      });
-    } catch (error) {
-      console.error("Error fetching survey data:", error);
-    }
-  };
-
-  useEffect(() => {
-    getCampCount();
-    getScreenedCount();
-  }, [catValue])
-
-
-
 
 
   return (
@@ -556,44 +228,8 @@ function Dashboard() {
           </div>
         </div>
 
-        {/* <div className="col-xl-2 col-md-4 mx-auto mb-4">
-          <div className="card border-left-primary shadow h-100 py-2">
-            <div className="card-body">
-              <div className="row no-gutters align-items-center">
-                <div className="col mr-2">
-                  <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                    Approval Completed
-                  </div>
-                  <div className="h5 mb-0 font-weight-bold text-gray-800">{totals.totalApprovalCompletedCount}</div>
-                </div>
-                <div className="col-auto">
-                  <i className="fas fa-user-check fa-2x text-gray-300"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> */}
       </div>
-      {/* <div className="graphflex">
-       <div className="graphdiv1">
-       <p className="camp-p">Camp Count</p>
-        <Bar data={chartData} options={options}></Bar>
-       </div>
-        <div className="graphdiv2"><Bar data={chartData1} options={options1}></Bar></div>  
-        </div>         */}
       <div className="card shadow mb-4">
-        {/* <div className="card-header py-3">
-          <button onClick={handelReportDownload} className="d-none d-sm-inline-block btn btn-sm btn-info shadow-sm">
-            <i className="fas fa-download fa-sm text-white-50"></i> Download Report
-          </button>
-          <button  onClick={() => {
-                                            
-                                          
-                                              handleOpenModal();
-                                            }} className="d-none m-1 d-sm-inline-block btn btn-sm btn-facebook shadow-sm">
-            <i className="fas fa-images fa-sm text-white-50"></i> Download Images
-          </button>
-        </div> */}
  <div className="row pt-5">
   {/* Recent Clients Table */}
   <div className="col-lg-5 mb-4">
@@ -613,7 +249,6 @@ function Dashboard() {
               </tr>
             </thead>
             <tbody>
-              {console.log("totals",totals)}
               {recentClients.map((client, i) => (
                 <tr key={i}>
                   <td>{client.client_name}</td>
@@ -648,19 +283,15 @@ function Dashboard() {
               </tr>
             </thead>
             <tbody>
-              {[
-                { client: "Apollo Hospitals", dept: "Cardiology", camp: "Heart Health Check-up Camp", start: "2025-10-12", end: "2025-10-13" },
-                { client: "Fortis Healthcare", dept: "General Medicine", camp: "Diabetes Screening Camp", start: "2025-10-10", end: "2025-10-11" },
-                { client: "Sunshine Diagnostics", dept: "Pathology", camp: "Blood Test Awareness Drive", start: "2025-10-08", end: "2025-10-09" },
-                { client: "CarePlus Clinic", dept: "Pediatrics", camp: "Child Health & Immunization Camp", start: "2025-10-06", end: "2025-10-07" },
-                { client: "Medicare Wellness", dept: "Physiotherapy", camp: "Posture & Spine Check Camp", start: "2025-10-04", end: "2025-10-05" },
-              ].map((camp, i) => (
+              {console.log("recentCamps : ",recentCamps)}
+              {recentCamps.map((camp, i) => (
                 <tr key={i}>
-                  <td>{camp.client}</td>
-                  <td>{camp.dept}</td>
-                  <td>{camp.camp}</td>
-                  <td>{camp.start}</td>
-                  <td>{camp.end}</td>
+                  <td>{camp.client_name}</td>
+                  <td>{camp.dept_name}</td>
+                  <td>{camp.camp_name}</td>
+                  <td>{new Date(camp.start_date).toLocaleDateString("en-GB")}</td>
+                  <td>{new Date(camp.end_date).toLocaleDateString("en-GB")}</td>
+
                 </tr>
               ))}
             </tbody>
