572518db158305ee9ca2f8228151cc84898c5b34
Suraj <sp990047@gmail.com>
2025-11-04 10:21:58 +0530

    admin constant changes

--- AdminPortal/src/components/constant/constant.js
+++ AdminPortal/src/components/constant/constant.js
@@ -1,6 +1,6 @@
 const devUrl = 'https://dynamiccampsapi.netcastservice.online'
 const localUrl = 'http://localhost:8035'
-export const BASEURL = localUrl
-export const BASEURL2 = localUrl
-export const CLIENTID = 3;
-export const DEPTID = 3;
+export const BASEURL = devUrl
+export const BASEURL2 = devUrl
+export const CLIENTID = 4;
+export const DEPTID = 4;
